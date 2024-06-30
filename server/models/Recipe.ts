754b9257a6fe7f03262d9e96035b58659bb73d22
Aser <ajhubero16@gmail.com>
2024-06-30 15:17:19 +0800

    Server - Added the model for Recipe

---
+++ server/models/Recipe.ts
@@ -0,0 +1,104 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+const DB_URL = process.env.DB_URL as string;
+
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("Connected to the database - RECIPE"))
+  .catch((err) => console.log("Error on recipe ", err));
+
+interface Ingredients {
+  ingredient: string;
+  unit: string;
+}
+
+interface IRecipe extends Document {
+  creator: Schema.Types.ObjectId;
+  title: string;
+  description: string;
+  images: string[];
+  saved: number;
+  ingredients: Ingredients[];
+  instructions: string[];
+  servings: number;
+  prepTime: {
+    hours: number;
+    min: number;
+  };
+  cookTime: {
+    hours: number;
+    min: number;
+  };
+}
+
+const recipeSchema: Schema<IRecipe> = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  images: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  saved: {
+    type: Number,
+    default: 0,
+  },
+  ingredients: [
+    {
+      ingredient: {
+        type: String,
+        required: true,
+      },
+      unit: {
+        type: String,
+        default: "",
+      },
+    },
+  ],
+
+  instructions: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  servings: {
+    type: Number,
+    required: true,
+  },
+  prepTime: {
+    hours: {
+      type: Number,
+      required: true,
+    },
+    min: {
+      type: Number,
+      required: true,
+    },
+  },
+  cookTime: {
+    hours: {
+      type: Number,
+      required: true,
+    },
+    min: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+
+const Recipe = mongoose.model<IRecipe>("Recipe", recipeSchema);
+
+export default Recipe;
