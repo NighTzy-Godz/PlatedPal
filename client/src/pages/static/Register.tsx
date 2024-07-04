4ea934a68775899dc08da35eab4ae31329329b51
Aser <ajhubero16@gmail.com>
2024-07-04 19:57:24 +0800

    Client - Added the responsiveness of register user

--- client/src/pages/static/Register.tsx
+++ client/src/pages/static/Register.tsx
@@ -36,120 +36,123 @@ function Register() {
 
   return (
     <div className="py-10 w-full h-[90dvh] grid place-items-center">
-      <div className="w-1/4 mx-auto ">
-        <div className="text-center mb-10">
-          <h1 className="text-mainColor text-3xl mb-2">Register</h1>
-          <p className="text-textColor text-lg leading-tight">
-            In order to join communities, add and save recipes, you must create
-            an account first
-          </p>
+      <div className="container mx-auto">
+        <div className=" 2xl:w-1/3 md:w-2/4 sm:w-3/4 sm:px-0 px-5 mx-auto ">
+          <div className="text-center mb-10">
+            <h1 className="text-mainColor text-3xl mb-2">Register</h1>
+            <p className="text-textColor text-lg leading-tight">
+              In order to join communities, add and save recipes, you must
+              create an account first
+            </p>
+          </div>
+          <form onSubmit={handleSubmit(handleRegisterUserSubmit)}>
+            <div className="flex sm:flex-nowrap flex-wrap gap-3 mb-5">
+              {" "}
+              <FormInputsContainer>
+                <InputLabel htmlFor="firstName" className="mb-1">
+                  First Name
+                </InputLabel>
+                <Input
+                  id="firstName"
+                  placeholder="Ex. John Doe . . ."
+                  {...register("firstName", {
+                    required: "First Name is a required field",
+                  })}
+                />
+                {errors.firstName && (
+                  <InputError errMsg={errors.firstName.message} />
+                )}
+              </FormInputsContainer>
+              <FormInputsContainer>
+                <InputLabel htmlFor="lastName" className="mb-1">
+                  Last Name
+                </InputLabel>
+                <Input
+                  id="lastName"
+                  placeholder="Ex. Elmerized. . ."
+                  {...register("lastName", {
+                    required: "Last Name is a required field",
+                  })}
+                />
+                {errors.lastName && (
+                  <InputError errMsg={errors.lastName.message} />
+                )}
+              </FormInputsContainer>
+            </div>
+            <div className="mb-5">
+              {" "}
+              <FormInputsContainer>
+                <InputLabel className="mb-1" htmlFor="email">
+                  Email
+                </InputLabel>
+                <Input
+                  {...register("email", {
+                    required: "Email is a required field",
+                  })}
+                  id="email"
+                  placeholder="Ex. johndoe@gmail.com"
+                  type="email"
+                />
+                {errors.email && <InputError errMsg={errors.email.message} />}
+              </FormInputsContainer>
+            </div>
+
+            <div className="flex gap-3 sm:flex-nowrap flex-wrap mb-5">
+              <FormInputsContainer>
+                <InputLabel htmlFor="password" className="mb-1">
+                  Password
+                </InputLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Your desired account password"
+                  {...register("password", {
+                    required: "Password is a required field",
+                    minLength: {
+                      value: 5,
+                      message: "Password should be atleast 5 characters",
+                    },
+                  })}
+                />
+                {errors.password && (
+                  <InputError errMsg={errors.password.message} />
+                )}
+              </FormInputsContainer>
+              <FormInputsContainer>
+                <InputLabel htmlFor="confirmPass" className="mb-1">
+                  Confirm Password
+                </InputLabel>
+                <Input
+                  id="confirmPass"
+                  type="password"
+                  placeholder="Match it with your password"
+                  {...register("confirmPassword", {
+                    required: "Confirm Password is a required field",
+                    minLength: {
+                      value: 5,
+                      message:
+                        "Confirm Password should be atleast 5 characters",
+                    },
+                  })}
+                />
+                {errors.confirmPassword && (
+                  <InputError errMsg={errors.confirmPassword.message} />
+                )}
+              </FormInputsContainer>
+            </div>
+
+            <div className="">
+              <Button
+                isLoading={isLoading}
+                type="submit"
+                variant="darkBlue"
+                className="w-full "
+              >
+                Register
+              </Button>
+            </div>
+          </form>
         </div>
-        <form onSubmit={handleSubmit(handleRegisterUserSubmit)}>
-          <div className="flex gap-3 mb-5">
-            {" "}
-            <FormInputsContainer>
-              <InputLabel htmlFor="firstName" className="mb-1">
-                First Name
-              </InputLabel>
-              <Input
-                id="firstName"
-                placeholder="Ex. John Doe . . ."
-                {...register("firstName", {
-                  required: "First Name is a required field",
-                })}
-              />
-              {errors.firstName && (
-                <InputError errMsg={errors.firstName.message} />
-              )}
-            </FormInputsContainer>
-            <FormInputsContainer>
-              <InputLabel htmlFor="lastName" className="mb-1">
-                Last Name
-              </InputLabel>
-              <Input
-                id="lastName"
-                placeholder="Ex. Elmerized. . ."
-                {...register("lastName", {
-                  required: "Last Name is a required field",
-                })}
-              />
-              {errors.lastName && (
-                <InputError errMsg={errors.lastName.message} />
-              )}
-            </FormInputsContainer>
-          </div>
-          <div className="mb-5">
-            {" "}
-            <FormInputsContainer>
-              <InputLabel className="mb-1" htmlFor="email">
-                Email
-              </InputLabel>
-              <Input
-                {...register("email", {
-                  required: "Email is a required field",
-                })}
-                id="email"
-                placeholder="Ex. johndoe@gmail.com"
-                type="email"
-              />
-              {errors.email && <InputError errMsg={errors.email.message} />}
-            </FormInputsContainer>
-          </div>
-
-          <div className="flex gap-3 mb-5">
-            <FormInputsContainer>
-              <InputLabel htmlFor="password" className="mb-1">
-                Password
-              </InputLabel>
-              <Input
-                id="password"
-                type="password"
-                placeholder="Your desired account password"
-                {...register("password", {
-                  required: "Password is a required field",
-                  minLength: {
-                    value: 5,
-                    message: "Password should be atleast 5 characters",
-                  },
-                })}
-              />
-              {errors.password && (
-                <InputError errMsg={errors.password.message} />
-              )}
-            </FormInputsContainer>
-            <FormInputsContainer>
-              <InputLabel htmlFor="confirmPass" className="mb-1">
-                Confirm Password
-              </InputLabel>
-              <Input
-                id="confirmPass"
-                type="password"
-                placeholder="Match it with your password"
-                {...register("confirmPassword", {
-                  required: "Confirm Password is a required field",
-                  minLength: {
-                    value: 5,
-                    message: "Confirm Password should be atleast 5 characters",
-                  },
-                })}
-              />
-              {errors.confirmPassword && (
-                <InputError errMsg={errors.confirmPassword.message} />
-              )}
-            </FormInputsContainer>
-          </div>
-
-          <div className="">
-            <Button
-              isLoading={isLoading}
-              type="submit"
-              variant="darkBlue"
-              className="w-full "
-            >
-              Register
-            </Button>
-          </div>
-        </form>
       </div>
     </div>
   );
